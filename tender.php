d6a05b97d25aea7ba64f30ec012a5d2cec6643d7
peterkoloo <petermaseno@gmail.com>
2022-03-08 09:05:01 +0300

    Add files via upload

---
+++ tender.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tenders</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="topnav">
+        <a href="index1.php"> <img src="img/logo.jpeg" width="50" height="50" alt="logo"></a>
+        <a class="active" href="index1.php">Home</a>
+        <a href="project.php">Government Tenders</a>
+        <a href="getinvolve.php">Parastatals Tenders</a>
+        <a href="tender.php">Learning Institutions Tenders</a>
+        <a href="jobs.php">NGO/iNGG</a>
+        <a href="gallary.php">Private Companies</a>
+      </div>
+      <div class="container">
+          <div class="row">
+            <div class="col-4">
+              <h4>Prequalifications: 2022/2023</h4>
+            </div>
+            <div class="col-4">
+              <h4>Youths/Women/PWD Tender Documents</h4>
+            </div>
+            <div class="col-4">
+              <h4>General Tenders Documents</h4>
+            </div>
+          </div>
+        </div>
+   
+      <footer>
+        <section class="mb-4">
+        </section>
+        <div class="container">
+          <div class="row">
+            <div class="col-2">
+              <h5 class="text-uppercase">Links</h5>
+              <a href="index1.php" class="text-white">Home</a>
+            </div>
+            <div class="col-2">
+              <h5 class="text-uppercase">Links</h5>
+              <a href="project.php" class="text-white">Government Tenders</a>
+            </div>
+            <div class="col-2">
+              <h5 class="text-uppercase">Links</h5>
+              <a href="getinvolve.php" class="text-white">Parastatals tenders</a>
+            </div>
+            <div class="col-2">
+              <h5 class="text-uppercase">Links</h5>
+              <a href="tender.php" class="text-white">Learning Institutions Tenders</a>
+            </div>
+            <div class="col-2">
+              <h5 class="text-uppercase">Links</h5>
+              <a href="jobs.php" class="text-white">NGO/iNGG</a>
+            </div>
+            <div class="col-2">
+              <h5 class="text-uppercase">Links</h5>
+              <a href="gallary.php" class="text-white">Private Companies</a>
+            </div>
+          </div>
+        </div>
+      
+      <table class="center">
+        <caption>Working Hours</caption>
+        <tr>
+          <th>Monday</th>
+          <th>Tuesday</th>
+          <th>Wednesday</th>
+          <th>Thursday</th>
+          <th>Friday</th>
+          <th>Saturday</th>
+          <th>Sunday</th>
+        </tr>
+        <tr>
+          <td>8:00-4:00</td>
+          <td>8:00-4:00</td>
+          <td>8:00-4:00</td>
+          <td>8:00-4:00</td>
+          <td>8:00-4:00</td>
+          <td>9:00-12:00</td>
+          <td>Closed</td>
+        </tr>
+      </table>
+      <!-- Copyright -->
+      <div class="text-center" style="background-color: rgba(0, 0, 0, 0.2);">
+        © 2022 Copyright:
+        <a class="text-white" href="https://koloofoundation.com/">Koloo Foundation</a>
+      </div>
+      <!-- Copyright -->
+    </footer>
+    <div class="footer">
+      <a href="#" class="fa fa-facebook"></a>
+      <a href="#" class="fa fa-twitter"></a>
+      <a href="#" class="fa fa-youtube"></a>
+    </div>
+    <!-- Footer -->
+      
+  </body>
+  </html>  
