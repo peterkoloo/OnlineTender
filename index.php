609b05afb8f2024ebe2341d669a0c970e9600bb2
Tosby <o2jose43@gmail.com>
2022-03-08 16:13:27 +0300

    added ui

--- index.php
+++ index.php
@@ -1,90 +1,146 @@
-<html>
-
-<head>
-    <title>KolosTender</title>
-    <!--======insert style.css file inside index.html=======-->
-    <link rel="stylesheet" href="css/styles.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-</head>
-
-<body>
-    <div class="containe<?php
-    session_start();
-?>
 <!DOCTYPE html>
 <html lang=" en">
 
-        <head>
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Welcome to Finance Portal</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="assets/plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400,700">
-            <title>Welcome to Finance Portal</title>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-            <link rel="stylesheet" href="assests/css/style.css">
-            <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
 
-        </head>
+<body class="hold-transition login-page">
+        <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#home">Login</a></li>
+            <li><a data-toggle="tab" href="#menu1">Register</a></li>
+        </ul>
+        <div class="tab-content">
+            <div id="home" class="tab-pane fade in active well">
+                <div class="login-box">
+                    <div class="login-logo">
+                        <a href="#">Finance Portal</a>
+                    </div>
+                    <!-- /.login-logo -->
+                    <div class="card">
+                        <div class="card-body login-card-body">
+                            <p class="login-box-msg">Sign in to start your session</p>
 
-        <body>
-            <div class="signup-form">
-                <form action="loginProcess.php" method="post" enctype="multipart/form-data">
-                    <h2>Login</h2>
-                    <p class="hint-text">Enter Login Details</p>
-                    <div class="form-group">
-                        <input type="email" class="form-control" name="email" placeholder="Email" required="required">
+                            <form action="login.php" method="post">
+                                <div class="input-group mb-3">
+                                    <input type="email" class="form-control" placeholder="Email">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-envelope"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input type="password" class="form-control" placeholder="Password">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-lock"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-8">
+                                        <div class="icheck-primary">
+                                            <input type="checkbox" id="remember">
+                                            <label for="remember">
+                                                Remember Me
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <!-- /.col -->
+                                    <div class="col-4">
+                                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                                    </div>
+                                    <!-- /.col -->
+                                </div>
+                            </form>
+                            <p class="mb-1">
+                                <a href="forgot-password.html">I forgot my password</a>
+                            </p>
+                        </div>
+                        <!-- /.login-card-body -->
                     </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" name="pass" placeholder="Password"
-                            required="required">
-                    </div>
-                    <div class="form-group">
-                        <button type="submit" name="save" class="btn btn-success btn-lg btn-block">Login</button>
-                    </div>
-                    <div class="text-center">Don't have an account? <a href="register.php">Register Here</a></div>
-                </form>
+                </div>
+                <!-- /.login-box -->
             </div>
-        </body>
+            <div id="menu1" class="tab-pane fade well">
+                <div class="register-box">
+                    <div class="register-logo">
+                        <a href="#">Finance Portal</a>
+                    </div>
+                    <div class="card">
+                        <div class="card-body register-card-body">
+                            <p class="login-box-msg">Register a new membership</p>
 
-</html>r">
-<div class="login-box">
-    <div class="row">
-        <div col-md-6 login-left>
-            <h2>Login Here</h2>
-
-            <form action="validation.php" method="post">
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" name="user" class="form-control" required>
+                            <form action="register.php" method="post">
+                                <div class="input-group mb-3">
+                                    <input type="text" class="form-control" placeholder="Full name">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-user"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input type="email" class="form-control" placeholder="Email">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-envelope"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input type="password" class="form-control" placeholder="Password">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-lock"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="input-group mb-3">
+                                    <input type="password" class="form-control" placeholder="Retype password">
+                                    <div class="input-group-append">
+                                        <div class="input-group-text">
+                                            <span class="fas fa-lock"></span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-8">
+                                        <div class="icheck-primary">
+                                            <input type="checkbox" id="agreeTerms" name="terms" value="agree">
+                                            <label for="agreeTerms">
+                                                I agree to the <a href="#">terms</a>
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <!-- /.col -->
+                                    <div class="col-4">
+                                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                                    </div>
+                                    <!-- /.col -->
+                                </div>
+                            </form>
+                        </div>
+                        <!-- /.form-box -->
+                    </div><!-- /.card -->
                 </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" class="form-control" required>
-                </div>
-                <button type="submit" class="btn btn-primary">Login</button>
-            </form>
+                <!-- /.register-box -->
+            </div>
         </div>
-        <div col-md-6 login-right>
-            <h2>Register Here</h2>
 
-            <form action="registration.php" method="post">
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" name="user" class="form-control" required>
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" name="password" class="form-control" required>
-                </div>
-                <button type="submit" class="btn btn-primary">Register</button>
-            </form>
-        </div>
-    </div>
-</div>
-</div>
 </body>
 
 </html>
